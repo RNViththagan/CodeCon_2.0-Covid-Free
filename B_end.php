1abf1e80b50ef560d7f3d1447807392636319790
RNViththagan <viththagan1999@gmail.com>
2021-08-22 09:09:43 +0530

    Php file name changed

--- B_end.php
+++ B_end.php
@@ -1,9 +1,52 @@
 <?php
 require 'config.php';
-//$_POST['Vaccination']=true;
-//unset($_POST['Vaccination']);
-//$_POST['email']="abcd@as.as";
 if(isset($_POST['Vaccination'])){
+    
+    $sql="SELECT email FROM vaccination_request "
+		." WHERE email='".$_POST['email']."'";
+	if(!$result = $con->query($sql))
+		{
+			echo $con->error;       
+		}
+	$wishid=$result->fetch_assoc();	
+	if(!$result->num_rows)
+	{
+		$check ="SELECT ID FROM vaccination_request "
+			." ORDER BY ID ASC";
+		$result=$con->query($check);
+        if($result->num_rows)
+        {
+            $lastid=null;
+            while($row = $result->fetch_assoc()) 
+            {
+                $lastid = $row['ID'];
+            }
+            $newIDno = substr($lastid, 4)+1;
+            $newIDno=str_pad($newIDno, 4, '0', STR_PAD_LEFT);
+            $newID="VC".$newIDno;
+        }
+        else{
+            $newID="VC0001";
+        }
+        echo $newID."<br>";
+		$sql="INSERT INTO vaccination_request (ID, email, first_name, last_name, phone, city, address) VALUES ('".$newID."', '".$_POST['email']."', '".$_POST['first_name']."', '".$_POST['last_name']."', '".$_POST['phone']."', '".$_POST['city']."', '".$_POST['address']."')";
+		if(!$result = $con->query($sql))
+		{
+			echo $con->error;       
+		}
+		else{
+			echo "<script>alert('your details entered!!')</script>";
+		}
+
+	}
+    else{
+        echo "<script>alert('Email already entered!!');
+        document.location = 'Vaccination-Request.html'</script>";
+    }
+    echo "<script>document.location = 'Covid-Free.html'</script>";
+}
+
+else if(isset($_POST['Need-Help'])){
     echo $_POST['first_name']."<br>";
     echo $_POST['last_name']."<br>";
     echo $_POST['email']."<br>";
